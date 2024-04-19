b3e1afa74a0b05640d79c0955310483dd113e0ae
matthieu.le.nozach@gmail.com <matthieu.le.nozach@gmail.com>
2024-04-19 15:54:12 +0200

    rewritten pod operator

--- dags/k8s_pod_operator.py
+++ dags/k8s_pod_operator.py
@@ -1,17 +1,31 @@
-
+from airflow import DAG
 from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
+from datetime import datetime, timedelta
 
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2023, 1, 1),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
 
-# KubernetesPodOperator example
+dag = DAG('kubernetes_pod_operator_dag',
+          default_args=default_args,
+          description='A DAG using KubernetesPodOperator',
+          schedule_interval=timedelta(days=1),
+          catchup=False)
+
 kubernetes_pod_operator = KubernetesPodOperator(
     namespace='default',
     image='python:3.8-slim',
     cmds=['python', '-c'],
     arguments=['print("Hello from the KubernetesPodOperator")'],
     name='airflow-kpo-task',
-    task_id='kpo_demo'
+    task_id='kpo_demo',
+    dag=dag
 )
-
-kubernetes_pod_operator.dry_run()
 
 kubernetes_pod_operator
